fa9edd72dcc40b4ae801bbf6f81cf8c683180151
Gawron97 <jakubgawron97@gmail.com>
2026-09-07 09:35:57 +0200

    set run name in config

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ def main():
     if not dataset_dirs:
         raise ValueError("Configure at least one dataset")
 
-    run_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    run_name = config["paths"]["run_name"]
     dataset_group = "_".join(sorted(dataset_dirs))
     run_dir = config["paths"]["output_root"] / dataset_group / run_name
     run_dir.mkdir(parents=True, exist_ok=False)
